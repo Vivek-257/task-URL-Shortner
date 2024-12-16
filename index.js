37cbd81904f398fb82efc556464fc8ae7e5d1012
Vivek <sharmav257@gmail.com>
2024-12-16 13:59:26 +0530

    adding google OAuth

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
-// require('dotenv').config()
 
-
-// const express = require('express');
-// const { connectToDatabase } = require('./src/utils/db.js');
-
-// const app = express();
-
-// (async () => {
-//   await connectToDatabase();
-
-//   app.listen(3000, () => {
-//     console.log("Server running on port 3000");
-//   });
-// })();
-
-
-const express = require('express');
 require('dotenv').config()
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const { connectToDatabase } = require('./src/utils/db.js');
 const urlRoutes = require('./src/routes/urlRoutes.js');
 
 const app = express();
-const PORT = 3000;
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const PORT= process.env.PORT
+
 
 app.use(express.json());
 app.use('/api', urlRoutes);
+
+
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: '/auth/google/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  console.log('Google profile:', profile);
+  done(null, profile); 
+}));
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the URL Shortener! <a href="/auth/google">Login with Google</a>');
+  });
+  
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.send(`Hello, ${req.user.displayName}! <a href="/logout">Logout</a>`);
+  }
+);
+
+app.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
 
 connectToDatabase().then(() => {
   app.listen(PORT, () => {
@@ -33,5 +66,6 @@ connectToDatabase().then(() => {
 }).catch((err) => {
   console.error('Failed to connect to database', err);
 });
+
 
 
